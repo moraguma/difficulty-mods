8a42e693bf456cfe6e416c5bb12fc18e06b8f191
Azure-Lazuline <azure@entanma.com>
2024-09-20 14:01:48 -0700

    fixed crash with enemy shots that had no damage info assigned, like monk trial turrets

--- src/enemy-damage.js
+++ src/enemy-damage.js
@@ -1,6 +1,8 @@
 ig.ENTITY.Player.inject({
 	onPreDamageModification(a,b,c,d,e,f) {
-		e.damage = Math.round(e.damage * sc.options.get("difficultymods-enemy-damage"));			
+		if(e && e.damage){
+			e.damage = Math.round(e.damage * sc.options.get("difficultymods-enemy-damage"));
+		}
 		return this.parent(a,b,c,d,e,f);
 	},
 });
